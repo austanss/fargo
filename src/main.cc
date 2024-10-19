721a19ddc80b3d145e7c49025f10413473aef944
austanss <austanssical@gmail.com>
2024-10-19 15:16:32 -0400

    repo: nuke

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,6 @@
-#include "exec/simulation.hh"
-#include "data/individual.hh"
 #include <memory>
 #include <minifb/minifb.h>
 
-using namespace fargo;
-
 int main() {
-    std::unique_ptr<SimulationHandle> sim_main = std::make_unique<SimulationHandle>("Main");
-
-    sim_main->reset(true);
-
-    while (sim_main->is_valid()) {
-        sim_main->tick();
-    }
-
-    // Redundant
-    sim_main.release();
+    return 0;
 }
